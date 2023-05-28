39e648ffacffad1a351f25fd7e400794c6794114
sian <chicori3@naver.com>
2023-05-28 14:17:36 +0900

    Initial commit

---
+++ src/main/java/com/example/number_baseball_server/NumberBaseballServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.number_baseball_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberBaseballServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberBaseballServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/number_baseball_server/NumberBaseballServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.number_baseball_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberBaseballServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
